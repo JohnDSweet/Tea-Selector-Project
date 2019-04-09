709d3ae3948847e3b554260c2b0072a0467bd210
Johndsweet <Johndsweet@hotmail.com>
2019-04-09 16:07:41 -0700

    update use interface javascript and add pictures of tea.

--- js/scripts.js
+++ js/scripts.js
@@ -3,37 +3,43 @@ var database = [
     flavor: 'none',
     style: 'energy',
     type: 'black',
-    rank:  0
+    rank:  0,
+    picture: "https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dwaecd507a/images/UXProject/new-tea-images/T125AE017_earl-grey-royale_r1.jpg"
   },
   { name: 'English Breakfast',
     flavor: 'none',
     style: 'earthy',
     type: 'black',
-    rank:  0
+    rank:  0,
+    picture: "https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dwaecd507a/images/UXProject/new-tea-images/T125AE017_earl-grey-royale_r1.jpg"
   },
   { name: 'Gunpowder Vanilla',
     type: 'green',
     style: 'light',
     flavor: 'vanilla',
-    rank:  0
+    rank:  0,
+    picture: "https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dw9eb03401/images/UXProject/new-tea-images/T115AE009_georgeous-geisha_r1.jpg?sw=262&sh=262&sm=fit"
   },
   { name: 'Dragon Ginger',
     type: 'green',
     style: 'earthy',
     flavor: 'ginger',
-    rank:  0
+    rank:  0,
+    picture: "https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dw9eb03401/images/UXProject/new-tea-images/T115AE009_georgeous-geisha_r1.jpg?sw=262&sh=262&sm=fit"
   },
   { name: 'Rose Hips',
     type: 'herbal',
     style: 'light',
     flavor: 'floral',
-    rank:  0
+    rank:  0,
+    picture: "https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dw5f441e61/images/UXProject/new-tea-images/T140AE119_watermelon-sorbet_r1.jpg?sw=262&sh=262&sm=fit"
   },
   { name: 'Vanilla Chamomile',
     type: 'herbal',
     style: 'light',
     flavor: 'vanilla',
-    rank:  0
+    rank:  0,
+    picture: "https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dw5f441e61/images/UXProject/new-tea-images/T140AE119_watermelon-sorbet_r1.jpg?sw=262&sh=262&sm=fit"
   }
 ];
 
@@ -66,13 +72,16 @@ Tea.prototype.getTea = function(tea) {
 
   var topRank = 0;
   var topName = [];
+  var topPicture = [];
   for (var i = 0; i < database.length; i++) {
     if (database[i].rank === topRank) {
       topName.push(database[i].name);
+      topPicture.push(database[i].picture);
     };
     if (database[i].rank > topRank) {
       topRank = database[i].rank;
       topName = [database[i].name];
+      topPicture = [database[i].picture];
     };
   };
 
@@ -80,20 +89,20 @@ Tea.prototype.getTea = function(tea) {
     database[i].rank = 0;
   };
 
-  $(".resultTea").empty().hide().addClass("coral").append(topName[0]).fadeIn(500);
+  $("#result1").empty().hide().addClass("coral").append(topName[0]).fadeIn(500);
+  $("#result2").empty();
+  $("#teaPicture2").empty()
+  $("#teaPicture1").empty().hide().append("<img id=\"teaPic\" src=\"" + topPicture[0] + "\">").fadeIn(1000);
+  console.log(topPicture);
 
   if (topName.length === 1) {
-    $(".result").empty().addClass("white").append("Our recommendation is...");
-    for (var i = 1; i < (topName.length); i++) {
-      $(".resultTea").append(" or " + topName[i]);
-    };
+    $(".resultTitle").empty().addClass("black").append("Our recommendation is...");
   };
 
   if (topName.length > 1) {
-    $(".result").empty().addClass("white").append("Our recommendations are...");
-    for (var i = 1; i < (topName.length); i++) {
-      $(".resultTea").append(" or<br><br><br>" + topName[i]);
-    };
+    $(".resultTitle").empty().addClass("black").append("Our recommendations are...");
+    $("#result2").empty().hide().addClass("coral").append(topName[1]).fadeIn(500);
+    $("#teaPicture2").empty().hide().append("<img id=\"teaPic\" src=\"" + topPicture[1] + "\">").fadeIn(1000);
   };
 };
 
@@ -121,6 +130,10 @@ $(document).ready(function() {
       $("#typeMenuButtons").addClass("hidden");
       $("#typeSelectedButton").empty().append("Black ⌄");
       $("#menuShade").fadeOut(500);
+      $("#teaPot").fadeOut(200);
+      setTimeout(function(){
+        $("#teaPot").removeClass("whiteTeaPotBackground").removeClass("greenTeaPotBackground").removeClass("herbalTeaPotBackground").addClass("blackTeaPotBackground").fadeIn(300);
+      }, 200);
       tea.getTea(tea);
     });
 
@@ -130,6 +143,10 @@ $(document).ready(function() {
       $("#typeMenuButtons").addClass("hidden");
       $("#typeSelectedButton").empty().append("Green ⌄");
       $("#menuShade").fadeOut(500);
+      $("#teaPot").fadeOut(200);
+      setTimeout(function(){
+        $("#teaPot").removeClass("whiteTeaPotBackground").removeClass("blackTeaPotBackground").removeClass("herbalTeaPotBackground").addClass("greenTeaPotBackground").fadeIn(300);
+      }, 200);
       tea.getTea(tea);
     });
 
@@ -139,6 +156,10 @@ $(document).ready(function() {
       $("#typeMenuButtons").addClass("hidden");
       $("#typeSelectedButton").empty().append("Herbal ⌄");
       $("#menuShade").fadeOut(500);
+      $("#teaPot").fadeOut(200);
+      setTimeout(function(){
+        $("#teaPot").removeClass("whiteTeaPotBackground").removeClass("greenTeaPotBackground").removeClass("blackTeaPotBackground").addClass("herbalTeaPotBackground").fadeIn(300);
+      }, 200);
       tea.getTea(tea);
     });
 
