b467bb347e7ee804a1ba09cd61f5617ba1674615
Johndsweet <Johndsweet@hotmail.com>
2019-04-10 16:17:06 -0700

    add prefrence button and improve datbase selection logic.

--- js/scripts.js
+++ js/scripts.js
@@ -51,18 +51,40 @@ function Tea() {
 
 var tea = new Tea();
 
-Tea.prototype.getTea = function(tea) {
-  for (var i = 0; i < database.length; i++) {
-    for (var j = 0; j <= tea.type.length; j++) {
-      if (tea.type[j] === database[i].type) {
-        database[i].rank += 10;
+Tea.prototype.getTea = function(tea, prefType) {
+// database comparison
+    for (var i = 0; i < database.length; i++) {
+      // No pref type comparison
+      if (prefType === "none") {
+        for (var j = 0; j <= tea.type.length; j++) {
+          if (tea.type[j] === database[i].type) {
+            database[i].rank += 0;
+          };
+        };
       };
-    };
+      // Pref Type comparison
+      if (prefType === "prefer") {
+        for (var j = 0; j <= tea.type.length; j++) {
+          if (tea.type[j] === database[i].type) {
+            database[i].rank += 15;
+          };
+        };
+      };
+      // Pref Type comparison
+      if (prefType === "require") {
+        for (var j = 0; j <= tea.type.length; j++) {
+          if (tea.type[j] === database[i].type) {
+            database[i].rank += 30;
+          };
+        };
+      };
+  // Style database comparison
     for (var j = 0; j <= tea.style.length; j++) {
       if (tea.style[j] === database[i].style) {
         database[i].rank += 10;
       };
     };
+  // Flavor datbase comparison
     for (var j = 0; j <= tea.flavor.length; j++) {
       if (tea.flavor[j] === database[i].flavor) {
         database[i].rank += 10;
@@ -85,28 +107,43 @@ Tea.prototype.getTea = function(tea) {
     };
   };
 
+  console.log("The preference is set to " + prefType);
+  console.log("The top rank is " + topName + " with " + topRank + " points.");
+
   for (var i = 0; i < database.length; i++) {
     database[i].rank = 0;
   };
 
-  $("#result1").empty().hide().addClass("coral").append(topName[0]).fadeIn(500);
-  $("#result2").empty();
-  $("#teaPicture2").empty()
-  $("#teaPicture1").empty().hide().append("<img id=\"teaPic\" src=\"" + topPicture[0] + "\">").fadeIn(1000);
-  console.log(topPicture);
+  if (tea.type.length === 0 && tea.style.length === 0 && tea.flavor.length === 0) {
 
-  if (topName.length === 1) {
-    $(".resultTitle").empty().addClass("black").append("Our recommendation is...");
+    $("#result1").empty();
+    $("#result2").empty();
+    $(".resultTitle").empty();
+    $("#teaPicture2").empty();
+    $("#teaPicture1").empty();
+
+    } else {
+
+    $("#result1").empty().hide().addClass("coral").append(topName[0]).fadeIn(1000);
+    $("#result2").empty();
+    $("#teaPicture2").empty()
+    $("#teaPicture1").empty().hide().append("<img id=\"teaPic\" src=\"" + topPicture[0] + "\">").fadeIn(1000);
+    if (topName.length === 1) {
+      $(".resultTitle").empty().addClass("black").append("Our recommendation is...");
+    };
+
+    if (topName.length > 1) {
+      $(".resultTitle").empty().addClass("black").append("Our recommendations are...");
+      $("#result2").empty().hide().addClass("coral").append(topName[1]).fadeIn(1000);
+      $("#teaPicture2").empty().hide().append("<img id=\"teaPic\" src=\"" + topPicture[1] + "\">").fadeIn(1000);
+    };
+
   };
 
-  if (topName.length > 1) {
-    $(".resultTitle").empty().addClass("black").append("Our recommendations are...");
-    $("#result2").empty().hide().addClass("coral").append(topName[1]).fadeIn(500);
-    $("#teaPicture2").empty().hide().append("<img id=\"teaPic\" src=\"" + topPicture[1] + "\">").fadeIn(1000);
-  };
 };
 
 $(document).ready(function() {
+  var prefType = "prefer";
 
   // buttons for type
 
@@ -128,39 +165,39 @@ $(document).ready(function() {
       tea.type.splice(0,1);
       tea.type.push($("#black").val());
       $("#typeMenuButtons").addClass("hidden");
-      $("#typeSelectedButton").empty().append("Black ⌄");
+      $("#typeSelectedButton").empty().append("Black tea");
       $("#menuShade").fadeOut(500);
-      $("#teaPot").fadeOut(200);
+      $("#teaPot").hide();
       setTimeout(function(){
-        $("#teaPot").removeClass("whiteTeaPotBackground").removeClass("greenTeaPotBackground").removeClass("herbalTeaPotBackground").addClass("blackTeaPotBackground").fadeIn(300);
-      }, 200);
-      tea.getTea(tea);
+        $("#teaPot").removeClass("whiteTeaPotBackground").removeClass("greenTeaPotBackground").removeClass("herbalTeaPotBackground").addClass("blackTeaPotBackground").fadeIn(400);
+      }, 100);
+      tea.getTea(tea, prefType);
     });
 
   $("#green").click(function() {
       tea.type.splice(0,1);
       tea.type.push($("#green").val());
       $("#typeMenuButtons").addClass("hidden");
-      $("#typeSelectedButton").empty().append("Green ⌄");
+      $("#typeSelectedButton").empty().append("Green tea");
       $("#menuShade").fadeOut(500);
-      $("#teaPot").fadeOut(200);
+      $("#teaPot").hide();
       setTimeout(function(){
-        $("#teaPot").removeClass("whiteTeaPotBackground").removeClass("blackTeaPotBackground").removeClass("herbalTeaPotBackground").addClass("greenTeaPotBackground").fadeIn(300);
-      }, 200);
-      tea.getTea(tea);
+        $("#teaPot").removeClass("whiteTeaPotBackground").removeClass("blackTeaPotBackground").removeClass("herbalTeaPotBackground").addClass("greenTeaPotBackground").fadeIn(400);
+      }, 100);
+      tea.getTea(tea, prefType);
     });
 
   $("#herbal").click(function() {
       tea.type.splice(0,1);
       tea.type.push($("#herbal").val());
       $("#typeMenuButtons").addClass("hidden");
-      $("#typeSelectedButton").empty().append("Herbal ⌄");
+      $("#typeSelectedButton").empty().append("Herbal tea");
       $("#menuShade").fadeOut(500);
-      $("#teaPot").fadeOut(200);
+      $("#teaPot").hide();
       setTimeout(function(){
-        $("#teaPot").removeClass("whiteTeaPotBackground").removeClass("greenTeaPotBackground").removeClass("blackTeaPotBackground").addClass("herbalTeaPotBackground").fadeIn(300);
-      }, 200);
-      tea.getTea(tea);
+        $("#teaPot").removeClass("whiteTeaPotBackground").removeClass("greenTeaPotBackground").removeClass("blackTeaPotBackground").addClass("herbalTeaPotBackground").fadeIn(400);
+      }, 100);
+      tea.getTea(tea, prefType);
     });
 
   // buttons for style
@@ -183,27 +220,27 @@ $(document).ready(function() {
       tea.style.splice(0,1);
       tea.style.push($("#light").val());
       $("#styleMenuButtons").addClass("hidden");
-      $("#styleSelectedButton").empty().append("Light ⌄");
+      $("#styleSelectedButton").empty().append("Light");
       $("#menuShade").fadeOut(500);
-      tea.getTea(tea);
+      tea.getTea(tea, prefType);
     });
 
   $("#earthy").click(function() {
       tea.style.splice(0,1);
       tea.style.push($("#earthy").val());
       $("#styleMenuButtons").addClass("hidden");
-      $("#styleSelectedButton").empty().append("Earthy ⌄");
+      $("#styleSelectedButton").empty().append("Earthy");
       $("#menuShade").fadeOut(500);
-      tea.getTea(tea);
+      tea.getTea(tea, prefType);
     });
 
   $("#energy").click(function() {
       tea.style.splice(0,1);
       tea.style.push($("#energy").val());
       $("#styleMenuButtons").addClass("hidden");
-      $("#styleSelectedButton").empty().append("Energy ⌄");
+      $("#styleSelectedButton").empty().append("Energy");
       $("#menuShade").fadeOut(500);
-      tea.getTea(tea);
+      tea.getTea(tea, prefType);
     });
 
   // buttons for flavor
@@ -222,7 +259,7 @@ $(document).ready(function() {
       $("#ginger").addClass("clicked");
       console.log("Value added to array. Type Array: " + tea.flavor)
     };
-    tea.getTea(tea);
+    tea.getTea(tea, prefType);
   });
 
   $("#floral").click(function() {
@@ -239,7 +276,7 @@ $(document).ready(function() {
       $("#floral").addClass("clicked");
       console.log("Value added to array. Type Array: " + tea.flavor)
     };
-    tea.getTea(tea);
+    tea.getTea(tea, prefType);
   });
 
   $("#vanilla").click(function() {
@@ -256,6 +293,67 @@ $(document).ready(function() {
       $("#vanilla").addClass("clicked");
       console.log("Value added to array. Type Array: " + tea.flavor)
     };
-    tea.getTea(tea);
+    tea.getTea(tea, prefType);
   });
+
+  // buttons for type preference
+  $("#typePrefSelectedButton").click(function() {
+    $("#typePrefMenuButtons").hide().removeClass("hidden").fadeIn(500);
+    $("#none").hide().fadeIn(150);
+    $("#prefer").hide();
+    setTimeout(function(){
+    $("#prefer").fadeIn(200);
+  }, 150);
+    $("#require").hide()
+    setTimeout(function(){
+    $("#require").fadeIn(200);
+  }, 300);
+    $("#menuShade").hide().removeClass("hidden").fadeIn(500);
+  });
+
+  $("#noneType").click(function() {
+      tea.type.splice(0,1);
+      // tea.type.push($("#black").val());
+      $("#typeSelectedButton").empty().hide().append("Type ⌄");
+      $("#typePrefMenuButtons").addClass("hidden");
+      $("#typePrefSelectedButton").empty().append("No preference");
+      $("#menuShade").fadeOut(500);
+      $("#teaPot").hide();
+      setTimeout(function(){
+        $("#teaPot").addClass("whiteTeaPotBackground").removeClass("greenTeaPotBackground").removeClass("herbalTeaPotBackground").removeClass("blackTeaPotBackground").fadeIn(400);
+      }, 100);
+      prefType = $("#noneType").val()
+      tea.getTea(tea, prefType);
+    });
+
+  $("#preferType").click(function() {
+      // tea.type.splice(0,1);
+      // tea.type.push($("#green").val());
+      $("#typeSelectedButton").fadeIn(500);
+      $("#typePrefMenuButtons").addClass("hidden");
+      $("#typePrefSelectedButton").empty().append("I prefer");
+      $("#menuShade").fadeOut(500);
+      // $("#teaPot").hide();
+      // setTimeout(function(){
+      //   $("#teaPot").removeClass("whiteTeaPotBackground").removeClass("blackTeaPotBackground").removeClass("herbalTeaPotBackground").addClass("greenTeaPotBackground").fadeIn(400);
+      // }, 100);
+      prefType = $("#preferType").val()
+      tea.getTea(tea, prefType);
+    });
+
+  $("#requireType").click(function() {
+      // tea.type.splice(0,1);
+      // tea.type.push($("#herbal").val());
+      $("#typeSelectedButton").fadeIn(500);
+      $("#typePrefMenuButtons").addClass("hidden");
+      $("#typePrefSelectedButton").empty().append("I require");
+      $("#menuShade").fadeOut(500);
+      // $("#teaPot").hide();
+      // setTimeout(function(){
+      //   $("#teaPot").removeClass("whiteTeaPotBackground").removeClass("greenTeaPotBackground").removeClass("blackTeaPotBackground").addClass("herbalTeaPotBackground").fadeIn(400);
+      // }, 100);
+      prefType = $("#requireType").val()
+      tea.getTea(tea, prefType);
+    });
+
 });
